3e4b9cde6f26a3fb99c00257a70ad82471c6c17d
Biel <gbrighini@gmail.com>
2024-06-19 21:33:57 -0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 // Defina o endereço de e-mail de recebimento
 $receiving_email_address = '0000998257@senaimgaluno.com.br';
 
-// Verifique se os dados do formulário foram enviados
+// Verifique se os dados do formulário foram enviados e o método de requisição é POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Coletar dados do formulário e aplicar escapes
     $from_name = htmlspecialchars(trim($_POST['name']));
@@ -10,15 +10,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subject = htmlspecialchars(trim($_POST['subject']));
     $message = htmlspecialchars(trim($_POST['message']));
     
-    // Limitar o tamanho máximo do campo de mensagem
-    $message = substr($message, 0, 1000);
-
     // Validar os dados do formulário
     if (empty($from_name) || empty($from_email) || empty($subject) || empty($message) || !filter_var($from_email, FILTER_VALIDATE_EMAIL)) {
-        // Dados inválidos, você pode configurar uma resposta adequada
+        // Dados inválidos, enviar resposta adequada
+        http_response_code(400); // Bad Request
         echo "Por favor, preencha o formulário corretamente.";
         exit;
     }
+    
+    // Limitar o tamanho máximo do campo de mensagem
+    $message = substr($message, 0, 1000);
 
     // Construir o corpo do e-mail
     $email_content = "Nome: $from_name\n";
@@ -32,12 +33,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Enviar o e-mail
     if (mail($receiving_email_address, $subject, $email_content, $email_headers)) {
+        http_response_code(200); // OK
         echo "Obrigado! Sua mensagem foi enviada.";
     } else {
+        http_response_code(500); // Internal Server Error
         echo "Oops! Algo deu errado e não conseguimos enviar sua mensagem.";
     }
 } else {
-    // Não é uma solicitação POST, você pode configurar uma resposta adequada
+    // Não é uma solicitação POST, enviar resposta adequada
+    http_response_code(405); // Method Not Allowed
     echo "Por favor, envie o formulário.";
 }
 ?>
